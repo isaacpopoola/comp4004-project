abc14f456c5a8b8d755c7007593200f8918b14a0
Ryan Duan <ryanduan@Ryans-MacBook-Pro.local>
2020-11-27 02:54:10 -0500

    TEST+CODE: get available courses

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -6,7 +6,7 @@ const Students = require("../db/models").Students;
 router.get("/all", async (req, res) => {
     try {
         const students = await Students.findAll();
-        if (students) return res.status(200).send({ students });
+        return res.status(200).send({ students });
     } catch {
         return res.status(400).send({ message: "error retrieving students" });
     }
